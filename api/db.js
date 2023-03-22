bcbdbdb441b884e07d8941850bd3e91b4ccd4a82
Neghxul <cgrr29us@gmail.com>
2023-03-22 14:36:26 -0600

    second commit

---
+++ api/db.js
@@ -0,0 +1,16 @@
+import mysql from "mysql";
+
+export const db = mysql.createConnection({
+    host: "localhost",
+    user:"root",
+    password:"password",
+    database:"blog"
+});
+
+db.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+  
+    console.log('Connected to the MySQL server.');
+  });
