aa7b6016de757eaaebde1d1a237043894c93eb3a
Francisco Riorda <riorda7@gmail.com>
2023-04-22 12:35:05 -0300

    actualización

--- src/components/SectionCateg/RenderingCat/RenderingCat.jsx
+++ src/components/SectionCateg/RenderingCat/RenderingCat.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./stylesRenderingCat.css";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const RenderingCat = ({ categoria }) => {
   AOS.init();
-  return (
-    <>
-      <div data-aos="fade-up" className="cardCategoria2" id="card">
-        <Link to={`${categoria.url}`} target={"_blank"} className="link">
-          <div className="divCard" data-aos="fade-right">
-            <ul className="ulCard">
-              <li className="liCard">{categoria.title}</li>
-              <li className="liCard authorCard">{categoria.author}</li>
-            </ul>
-          </div>
-        </Link>
-      </div>
-    </>
+  return !categoria ? (
+    <h1>API no disponible en deploy</h1>
+  ) : (
+    <div data-aos="fade-up" className="cardCategoria2" id="card">
+      <Link to={`${categoria.url}`} target={"_blank"} className="link">
+        <div className="divCard" data-aos="fade-right">
+          <ul className="ulCard">
+            <li className="liCard">{categoria.title}</li>
+            <li className="liCard authorCard">{categoria.author}</li>
+          </ul>
+        </div>
+      </Link>
+    </div>
   );
 };
 
